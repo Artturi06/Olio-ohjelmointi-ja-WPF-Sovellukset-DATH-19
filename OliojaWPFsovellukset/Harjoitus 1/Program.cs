dceb8068e682c2ad41e1f53cc0511e4e722b97d1
Artturi Arajärvi <artturi.arajarvi@student.careeria.fi>
2023-03-30 13:23:04 +0300

    projekti commit

--- OliojaWPFsovellukset/Harjoitus 1/Program.cs
+++ OliojaWPFsovellukset/Harjoitus 1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehilce VehicleOlio = new Vehilce();
+
+
+            VehicleOlio.Nimi = "Volvo";
+            VehicleOlio.Nopeus = 120;
+            VehicleOlio.Renkaat = 4;
+
+
+            Console.WriteLine(VehicleOlio.Haetiedot());
+        }
+    }
+}
---
+++ OliojaWPFsovellukset/Harjoitus 1/Vehilce.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_1
+{
+    
+    public class Vehilce
+    {
+        public string Nimi { get; set; }
+        public int Nopeus { get; set; }
+        public int Renkaat { get; set; }
+
+        public string Haetiedot()
+        {
+            string Ajoneuvotiedot = "nimi: " + Nimi + ".  Nopeus: " + Nopeus.ToString() + ".  Renkaat: " + Renkaat.ToString();
+
+            return Ajoneuvotiedot;
+        }
+    }
+
+
+
+
+}
