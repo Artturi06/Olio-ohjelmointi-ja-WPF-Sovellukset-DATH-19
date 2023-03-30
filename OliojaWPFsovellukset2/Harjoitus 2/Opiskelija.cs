0e294a54e10a7f1bfe6bd061cdcd35ed4bea4510
Artturi Arajärvi <artturi.arajarvi@student.careeria.fi>
2023-03-30 15:25:55 +0300

    projekti commit

---
+++ OliojaWPFsovellukset2/Harjoitus 2/Opiskelija.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_2
+{
+    internal class Opiskelija
+    {
+        public string Nimi { get; set; }
+        public int OppID { get; set; }
+        public int Opintopisteet { get; set; }
+
+
+
+
+        public Opiskelija(string _nimi, int _Oppid, int _Opintopisteet)
+        {
+            Nimi = _nimi;
+            OppID = _Oppid;
+            Opintopisteet = _Opintopisteet;
+        }
+
+        public string Oppilashaku1()
+        {
+            string Oppilas = "Nimi: " + Nimi + ".  OppID: " + OppID.ToString() + ".  Opintopisteet: " + Opintopisteet.ToString();
+            
+            return Oppilas;
+        }
+
+        public string Oppilashaku2()
+        {
+            string Oppilas1 = "Nimi: " + Nimi + ".  OppID: " + OppID.ToString() + ".  Opintopisteet: " + Opintopisteet.ToString();
+
+            return Oppilas1;
+        }
+
+
+    }
+}
---
+++ OliojaWPFsovellukset2/Harjoitus 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija OpiskelijaTiedot = new Opiskelija("Matti", 1, 200);
+            Opiskelija Opiskelijatiedot2 = new Opiskelija("Taneli", 2, 180);
+
+
+
+
+            Console.WriteLine(OpiskelijaTiedot.Oppilashaku1());
+            Console.WriteLine(Opiskelijatiedot2.Oppilashaku2());
+            
+        }
+    }
+}
