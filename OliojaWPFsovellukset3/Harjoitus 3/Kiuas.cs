3215c14e0d283a2b6d51dcc5b682ab2dc02497b8
Artturi Arajärvi <artturi.arajarvi@student.careeria.fi>
2023-03-31 14:55:17 +0300

    projekti commit

---
+++ OliojaWPFsovellukset3/Harjoitus 3/Kiuas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_3
+{
+    internal class Kiuas
+    {
+        public string Virta { get; set; }
+        public int Lampotila { get; set; }
+        public int Kosteusarvo { get; set; }
+
+
+        public Kiuas(string _Virta, int _lampotila, int _kosteusarvo)
+        {
+            Virta= _Virta;
+            Lampotila = _lampotila;
+            Kosteusarvo= _kosteusarvo;
+        }
+
+
+
+        public string KiukaanTiedot()
+        {
+            string KiuasTiedot = ("Tila: " + Virta + ".  Lampotila: " + Lampotila + ".  Kosteusarvo: " + Kosteusarvo + "%");
+            
+            return KiuasTiedot;
+        }
+    }
+}
---
+++ OliojaWPFsovellukset3/Harjoitus 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas kiuas1 = new Kiuas("Päällä", 80, 90 );
+
+            Console.WriteLine(kiuas1.KiukaanTiedot());
+
+            kiuas1.Virta = "Pois";
+            kiuas1.Lampotila = 100;
+            kiuas1.Kosteusarvo = 95;
+
+            Console.WriteLine(kiuas1.KiukaanTiedot());
+            
+
+        }
+
+        
+    }
+}
