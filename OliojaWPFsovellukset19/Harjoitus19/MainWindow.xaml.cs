911e6b6b8ea1bf8577fdec6b0b6891dc27b448fa
Artturi Arajarvi <Artturi.arajarvi@gmail.com>
2023-05-17 12:40:04 +0300

    projekti commit

---
+++ OliojaWPFsovellukset19/Harjoitus19/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HissiWPF
+{
+    public partial class MainWindow : Window
+    {
+        private int kerros;
+
+        public int Kerros
+        {
+            get { return kerros; }
+            set { kerros = value; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button != null)
+            {
+                int valittuKerros = Int32.Parse(button.Content.ToString());
+                if (valittuKerros >= 1 && valittuKerros <= 6)
+                {
+                    Kerros = valittuKerros;
+                    MessageBox.Show("Siirrytään kerrokseen " + Kerros);
+                }
+                else
+                {
+                    MessageBox.Show("Virheellinen kerrosvalinta!");
+                }
+            }
+        }
+    }
+}
